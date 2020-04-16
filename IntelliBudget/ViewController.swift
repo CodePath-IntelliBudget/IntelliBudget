70ac245e2d949dfe9c6a5c02cc817cf97a7e3755
Dennis Krupitsky <d.krupitsky@gmail.com>
2020-04-16 14:19:27 -0500

    initial commit

---
+++ IntelliBudget/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IntelliBudget
+//
+//  Created by user165734 on 4/16/20.
+//  Copyright © 2020 user165734. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
