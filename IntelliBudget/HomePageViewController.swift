c81da9817bca9af2a143a223e976d8d2c553da93
Joseph DeMario <jd730@icloud.com>
2020-04-19 18:00:01 -0400

    register/login

---
+++ IntelliBudget/HomePageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomePageViewController.swift
+//  IntelliBudget
+//
+//  Created by Joseph  DeMario on 4/19/20.
+//  Copyright © 2020 user165734. All rights reserved.
+//
+
+import UIKit
+
+class HomePageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
