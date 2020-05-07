7da2ce17fb3689add3b5232eac00df1551dbe9d4
Joseph Tooker <tookerjt@gmail.com>
2020-05-07 15:31:03 -0400

    attempted to auto update the chart

--- IntelliBudget/ChartViewController.swift
+++ IntelliBudget/ChartViewController.swift
@@ -21,8 +21,6 @@ class ChartViewController: UIViewController, ChartViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //barChart.delegate = self
-        //lineChart.delegate = self
         pieChart.delegate = self
 
         // Do any additional setup after loading the view.
@@ -30,9 +28,11 @@ class ChartViewController: UIViewController, ChartViewDelegate {
     
     // query for the purchases in the database
     override func viewDidAppear(_ animated: Bool) {
-           super.viewDidAppear(animated)
-           
-       
+            super.viewDidAppear(animated)
+        
+        //attempt to auto update the chart
+            pieChart.clearValues()
+            self.viewDidLayoutSubviews()
            
     }
     
@@ -90,68 +90,6 @@ class ChartViewController: UIViewController, ChartViewDelegate {
 
 
     }
-    /*
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        
-        barChart.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.width)
-        barChart.center = view.center
-        view.addSubview(barChart)
-        
-        var entries = [BarChartDataEntry]()
-        
-        for x in 0..<10 {
-            entries.append(BarChartDataEntry(x:Double(x), y:Double(x)))
-        }
-        
-        let set = BarChartDataSet(entries: entries)
-        set.colors = ChartColorTemplates.joyful()
-        
-        let data = BarChartData(dataSet: set)
-        
-        barChart.data = data
-    }
-    */
-    
-    /*
-    override func viewDidLayoutSubviews() {
-        lineChart.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.width)
-        lineChart.center = view.center
-        view.addSubview(lineChart)
-               
-        var entries = [ChartDataEntry]()
-               
-        for x in 0..<10 {
-            entries.append(ChartDataEntry(x:Double(x), y:Double(x)))
-        }
-               
-        let set = LineChartDataSet(entries: entries)
-        set.colors = ChartColorTemplates.material()
-               
-        let data = LineChartData(dataSet: set)
-               
-        lineChart.data = data
-    }
-     
-     override func viewDidLayoutSubviews() {
-        pieChart.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.width)
-        pieChart.center = view.center
-        view.addSubview(pieChart)
-            
-        var entries = [ChartDataEntry]()
-            
-        for x in 0..<10 {
-            entries.append(BarChartDataEntry(x:Double(x), y:Double(x)))
-        }
-            
-        let set = PieChartDataSet(entries: entries)
-        set.colors = ChartColorTemplates.joyful()
-            
-        let data = PieChartData(dataSet: set)
-            
-        pieChart.data = data
-     }
-    */
 
 
     /*
